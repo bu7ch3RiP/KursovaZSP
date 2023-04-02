962f30b1060628dac57e233fef805ee478507a9b
Vitalii Romaniv <vitalii.romaniv03@gmail.com>
2023-04-02 16:01:56 +0300

    add selectAll and clearAll function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QList>
+#include <QCheckBox>
+#include <QAction>
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -15,4 +18,55 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+
+void MainWindow::on_CalibrationTests_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+
+}
+
+
+void MainWindow::on_GridTests_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_ColorTests_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_ReadingTests_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+
+void MainWindow::on_actionSelect_all_triggered()
+{
+    for (int i = 0; i < ui->stackedWidget->count(); i++)
+    {
+        QList<QCheckBox*> checkBoxes = ui->stackedWidget->widget(i)->findChildren<QCheckBox*>();
+        for (int j = 0; j < checkBoxes.count(); j++)
+        {
+            checkBoxes.at(j)->setChecked(true);
+        }
+    }
+}
+
+
+void MainWindow::on_actionClear_All_triggered()
+{
+    for (int i = 0; i < ui->stackedWidget->count(); i++)
+    {
+        QList<QCheckBox*> checkBoxes = ui->stackedWidget->widget(i)->findChildren<QCheckBox*>();
+        for (int j = 0; j < checkBoxes.count(); j++)
+        {
+            checkBoxes.at(j)->setChecked(false);
+        }
+    }
+}
 
--- mainwindow.h
+++ mainwindow.h
@@ -19,6 +19,16 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_CalibrationTests_clicked();
+    void on_GridTests_clicked();
+    void on_ColorTests_clicked();
+    void on_ReadingTests_clicked();
+
+    void on_actionSelect_all_triggered();
+
+    void on_actionClear_All_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
